ed51c2c5c6471b74feea942e362e06764dc5f2d5
sj245 <john.yoon@emthink.com>
2016-08-26 14:38:49 +0900

    Just decode function works but don't know is true

---
+++ HomeBot/HomeBot.ino
@@ -0,0 +1,62 @@
+// ESP8266_Blynk_Aircon (2016.8.13)
+ 
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+ 
+char auth[] = "";   // your Blynk Auth Token
+const char* ssid = "";                         // your ssid
+const char* password = "";                // your password
+ 
+int onSignal[]  = {3100,-9800,470,-1550,470,-550,470,-550,470,-550,470,-1550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-1550,470,-1550,470,-550,470,-550,470,-550,470,-1550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,0};
+int offSignal[] = {3100,-9800,470,-1550,470,-550,470,-550,470,-550,470,-1550,470,-550,470,-550,470,-550,470,-1550,470,-1550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-550,470,-1550,470,-550,470,-1550,470,-550,470,-550,470,-550,470,-1550,470,0};
+int testSignal[] = {9000,-4500,500,-500,500,-500,500,-1500,500,-500,500,-500,500,-500,500,-500,500,-500,500,-1500,500,-1500,500,-500,500,-1500,500,-1500,500,-1500,500,-1500,500,-1500,500,-500,500,-500,500,-1500,500,-500,500,-500,500,-500,500,-1500,500,-500,500,-1500,500,-1500,500,-500,500,-1500,500,-1500,500,-1500,500,-500,500,-1500,500,0};
+ 
+int pinBlynk = 2;                                   // onboard LED pin of ESP-12F
+int pinRemote = 5;                                  // GPIO pin 5 (D1 of NodeMCU, WeMos D1 Mini)
+int airconStatus;
+ 
+void setup()
+{
+  Blynk.begin(auth, ssid, password);
+
+  pinMode(pinRemote,OUTPUT);
+  airconStatus = digitalRead(pinBlynk);
+}
+ 
+void loop()
+{
+  Blynk.run();
+ 
+  if(airconStatus == !digitalRead(pinBlynk)) {
+    if(digitalRead(pinBlynk)==LOW) {
+      IRsend(pinRemote,onSignal);     
+    }
+    else {
+      IRsend(pinRemote,offSignal);
+    }
+    airconStatus = digitalRead(pinBlynk);
+  }
+}
+ 
+void IRsend(int IRpin, int IRsignal[]) {
+  int i=0;
+  pinMode(IRpin, OUTPUT);
+  do {
+    if (IRsignal[i]>0) {
+      int duration = IRsignal[i];
+      while (duration > 0) {
+        digitalWrite(IRpin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(IRpin, LOW);
+        delayMicroseconds(10);
+        duration -= 26;         // 38 kHz = 26.3 uSec
+      }
+    }
+    else {
+      delayMicroseconds(abs(IRsignal[i]));
+    }
+    i+=1;
+  }
+  while (IRsignal[i] != 0);
+}
+
---
+++ RawIRdecode_Mod/RawIRdecode_Mod.ino
@@ -0,0 +1,53 @@
+/*
+ * RawIRdecode_Mod_v2 (2016.8.20)
+ * 
+ * Original Sketch --> https://github.com/adafruit/Raw-IR-decoder-for-Arduino
+ */
+ 
+unsigned int pinIR = 16;            // IR sensor pin ===> D0
+unsigned int currentPulse = 0;     // Pulse index
+unsigned long pulses[100][2];      // Array of high and low pulse 
+unsigned long maxPulse = 50000;    // Maximum listen duration (65 msec)
+ 
+ 
+void setup(void) {
+  wdt_disable();
+  Serial.begin(9600);
+  Serial.println("Ready to decode IR!");
+  
+}
+ 
+ 
+void loop(void) {
+  unsigned long highPulse = 0;     // Pulse length
+  unsigned long lowPulse = 0;
+  unsigned long currTime = micros();
+  
+  while (!digitalRead(pinIR)) {
+    highPulse = micros()-currTime;
+    if ((highPulse >= maxPulse) && (currentPulse != 0)) {
+      printPulses();
+      currentPulse = 0;
+    }
+  }
+  pulses[currentPulse][0] = highPulse;
+  currTime = micros();
+   
+  while (digitalRead(pinIR)) {
+    lowPulse = micros()-currTime;
+  }
+  pulses[currentPulse][1] = lowPulse;
+  currentPulse++;
+}
+ 
+ 
+void printPulses(void) {
+  for (int i = 0; i < currentPulse-1; i++) {
+    Serial.print(pulses[i][1], DEC);
+    Serial.print(",-");
+    Serial.print(pulses[i+1][0], DEC);
+    Serial.print(",");
+  }
+  Serial.println(pulses[currentPulse-1][1], DEC);
+}
+
